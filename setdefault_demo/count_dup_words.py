8a60367ccbec9716b1956570f57814f19367a863
zabuchan <4702689+zabuchan@users.noreply.github.com>
2018-12-26 22:23:37 +0800

    Add files via upload

--- setdefault_demo/count_dup_words.py
+++ setdefault_demo/count_dup_words.py
@@ -1,33 +1,30 @@
 
 import re
-import sys
+import os
 
 WORE_RE = re.compile(r'\w+')
-
+file_name = os.path.join(os.path.dirname(__file__),
+                        "harry_potter_sorcerer's_stone_1st_chapter.txt")
 index = {}
-# with open(sys.argv[1], 'r') as fp:
+
+# with open(file_name, 'r') as fp:
 # 	for line_no, line in enumerate(fp, 1):
 # 		for match in WORE_RE.finditer(line):
 # 			word = match.group()
-# 			#print(match.start())
-# 			# print(word)
 # 			column_no = match.start()+1
-# 			location = {line_no, column_no}
+# 			location = (line_no, column_no)
 # 			occurences = index.get(word, [])
 # 			occurences.append(location)
 # 			index[word] = occurences
 # 			# print(occurences)
-
-with open(sys.argv[1], 'r') as fp:
+# #
+with open(file_name, 'r') as fp:
 	for line_no, line in enumerate(fp, 1):
 		for match in WORE_RE.finditer(line):
 			word = match.group()
-			#print(match.start())
-			# print(word)
 			column_no = match.start()+1
 			location = (line_no, column_no)
 			index.setdefault(word, []).append(location)
 
-
 for word in sorted(index,key=str.upper):
-	print(word, index[word])
+	print("Word: {}, Word Count: {}, Location: {}".format(word, len(index[word]), index[word]))
